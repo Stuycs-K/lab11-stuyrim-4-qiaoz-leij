84544a13588182abbecbd00ad3ccf7022156d819
Zixi Qiao <qiaozixi@gmail.com>
2025-01-11 11:52:51 -0500

    updated spiderswarm to extend general enemies class

--- SpiderSwarm.java
+++ SpiderSwarm.java
@@ -1,93 +1,25 @@
-public class SpiderSwarm extends Adventurer {
-
-    private Status poisoned;
-    private Status paralyzed;
-    private int webbing, maxWebbing;
+public class SpiderSwarm extends Enemies {
 
     public SpiderSwarm() {
-        super("Spider Swarm", 20);
-
-        poisoned = new Status("Poisoned", 3);
-        paralyzed = new Status("Paralyzed", 2);
-
-        webbing = 1;
-        maxWebbing = 5;
-    }
-
-    @Override
-    public String getSpecialName() {
-        return "Webbing";
-    }
-
-    @Override
-    public int getSpecial() {
-        return webbing;
-    }
-
-    @Override
-    public int getSpecialMax() {
-        return maxWebbing;
-    }
-
-    @Override
-    public void setSpecial(int n) {
-      webbing = n;
+        super("Spider Swarm", 20, "Webbing", 1, 5);
     }
 
     @Override
     public String attack(Adventurer other) {
         int dmg = (int) (Math.random() * 6) + 1;
         other.applyDamage(dmg);
-        poisoned.set(3);
+        other.applyCondition("Poisoned", 3);
         int n = restoreSpecial(1);
         return "Dealt " + dmg + " damage; Restored " + n + " " + getSpecialName();
     }
 
-    private int heal() {
-        int heal;
-        heal = (int) (Math.random() * 6) + (int) (Math.random() * 6) + 2;
-        if (Math.random() < 0.5) {
-            heal = -heal;
-        }
-        return heal;
-    }
-    
-    @Override
-    public String support(Adventurer other) {
-        int heal = heal();
-        String healMsg;
-        if (heal > 0) {
-            if (other.getHP() + heal < other.getmaxHP()) {
-                other.setHP(other.getHP() + heal);
-                healMsg = "Healed " + other.getName() + " " + heal + "HP";
-              } else {
-                other.setHP(other.getmaxHP());
-                healMsg = "At max HP";
-              }
-        } else {
-            if (other.getHP() + heal > 0) {
-                other.setHP(other.getHP() + heal);
-                healMsg = other.getName() + " Lost " + heal + "HP";
-              } else {
-                other.setHP(0);
-                healMsg = "Dead";
-              }
-        }
-        return healMsg;
-    }
-
-    @Override
-    public String support() {
-        return support(this);
-    }
-
     @Override
     public String specialAttack(Adventurer other) {
-        if (webbing < 3) {
+        if (getSpecial() < 3) {
             return "Too little mana; " + attack(other);
           } else {
-            webbing -= 3;
-            paralyzed.set(2);
+            setSpecial(getSpecial()-3);;
+            other.applyCondition("Paralyzed", 2);
           }
         return "Paralyzed " + other.getName() + " for 2 turns";
     }
