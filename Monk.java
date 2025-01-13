849c9845b5679c17a976fba608cea0baac811eee
James Lei <bestpsyduckever@gmail.com>
2025-01-13 11:01:18 -0500

    Final in-class commit

---
+++ Monk.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Monk extends Adventurer {
+  private final ArrayList<String> vulnerabilities = {};
+  private final ArrayList<String> resistances = {"Bludgeoning, Psychic"}
+  public Monk(String name) {
+    super(name, 30, vulnerabilities, resistances);
+  }
+  public String getSpecialName().
+
+}
